c69c4eb1bae24c8064e9cf1d565f4967ef31a5ae
SHR <s.hawkinsrobertson@gmail.com>
2022-05-05 11:21:08 -0700

    add taskmgr class, constructor, addTask method

---
+++ js/taskmanager.js
@@ -0,0 +1,11 @@
+class taskManager {
+   constructor(task, currentId) {
+       this.task = [];
+       this.currentId = 0;
+
+       addTask(name, description, assignedTo, dueDate, status) {
+           this.currentId ++;
+           this.task.push()
+       }
+   }
+}
