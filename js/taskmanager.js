d319cccfddcca9f9bcf4ad1acf2b4ce86b55f4cd
SHR <s.hawkinsrobertson@gmail.com>
2022-05-05 11:45:31 -0700

    debug tM method

--- js/taskmanager.js
+++ js/taskmanager.js
@@ -2,11 +2,12 @@ class taskManager {
    constructor(task, currentId) {
        this.task = [];
        this.currentId = 0;
+    }
 
-       addTask(name, description, assignedTo, dueDate, status) {
-           let status = 'TODO';
-           this.currentId ++;
-           this.task.push(this.currentId, name, description, assignedTo, dueDate, status);
+    addTask(name, description, assignedTo, dueDate){
+        let status = 'TODO';
+        this.currentId ++;
+        this.task.push(this.currentId, name, description, assignedTo, dueDate, status);
        }
-   }
+  
 }
