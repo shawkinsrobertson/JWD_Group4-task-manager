c7ec8fe915f035e3ddb6676c9671d19e0f11e510
SHR <s.hawkinsrobertson@gmail.com>
2022-05-05 17:13:54 -0700

    createTaskHtml function

--- js/taskmanager.js
+++ js/taskmanager.js
@@ -1,15 +1,16 @@
 const createTaskHtml = (name, description, assignedTo, dueDate, status) => {
-    <li class="list-group-item">
+   return `<li class="list-group-item">
                             <div class="card border-danger mb-3" style="max-width: 18rem;">
-                            <div class="card-header bg-transparent border-danger font-weight-bold">Task Name</div>
+                            <div class="card-header bg-transparent border-danger font-weight-bold"> ${name} </div>
                             <div class="card-body text-danger">
-                                 <p class="card-text">Description</p>
-                                 <p class="card-text">Assigned to:</p>
-                                 <p class="card-text">Due Date:</p>
+                                 <p class="card-text"> ${description} </p>
+                                 <p class="card-text"> ${assignedTo} </p>
+                                 <p class="card-text"> ${dueDate} </p>
                             </div>
                             <div class="card-footer bg-transparent border-danger"><button type="button" class="btn btn-primary">▶︎</button>
                             <button type="button" class="btn btn-success">✓</button></div>
-                        </li>
+                        </li>`;
+
 }
 
 class taskManager {
